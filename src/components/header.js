87ce693b0555f0a6975fc4820c10443858f8995a
FrancescoSpinella3 <francescospinella76@gmail.com>
2025-12-22 16:43:36 +0100

    update

--- src/components/header.js
+++ src/components/header.js
@@ -26,7 +26,7 @@ export function Header() {
     nav.appendChild(navLogo);
 
     const navLinks = document.createElement('ul');
-    navLinks.className = "hidden gap-5 text-lg font-medium text-(--light-text-color) lg:flex *:relative *:after:content-[''] *:after:absolute *:after:left-0 *:after:bottom-[-34px] *:after:w-0 *:after:h-[3px] *:after:bg-white *:after:transition-all *:after:duration-300 *:hover:after:w-full *:z-10";
+    navLinks.className = "hidden gap-5 text-lg font-medium text-(--light-text-color) lg:flex *:relative *:after:content-[''] *:after:absolute *:after:left-0 *:after:bottom-[-34px] *:after:w-0 *:after:h-[3px] *:after:bg-white *:after:transition-all *:after:duration-500 *:hover:after:w-full *:z-10";
     navLinks.innerHTML = `
         <li id="home"><a href="/">HOME</a></li>
         <li id="stagione">
